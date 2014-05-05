3706f59d11ccfe403bb422833f035c291d1a9347
Josh Lind <jlind@tableausoftware.com>
2014-05-04 18:40:07 -0700

    use SPI to output force to color

--- Blink_Trinket.ino
+++ Blink_Trinket.ino
@@ -2,16 +2,26 @@
  * Sense and blink.
  */
 
+#include "SPI.h"
+#include "WS2801.h"
+
 // Trinket built in LED is 1, standard Arduinos use 13.
-int test_led = 13; // Trinket built in LED.
+int test_led = 1; // Trinket built in LED.
+int dataPin = 7;
+int clockPin = 8;
+
+int wait = 300;
 boolean DEBUG = true;
+int color_index = 0;
+WS2801 strip = WS2801(1, dataPin, clockPin);
 
 // Basic setup.
 void setup() {                
   // Initialize the digital pin as an output.
   pinMode(test_led, OUTPUT);
-  pinMode(2, INPUT);
-  
+  pinMode(A2, INPUT);
+  strip.begin();
+
   if (DEBUG) {
     Serial.begin(9600);
   }
@@ -20,27 +30,25 @@ void setup() {
 // Master looper.
 void loop() {
   //float flexiforce_reading = 1.0;
-  int flexiforce_reading = analogRead(1);
+  int flexiforce_reading = analogRead(A2);
   //int blink_delay = map(flexiforce_reading, 0, 1023, 30, 1200);
-  float blink_delay = fscale(0, 1023, 400, 10, flexiforce_reading, 8);
+  flexiforce_reading = flexiforce_reading - (flexiforce_reading % 25);
+  float color = fscale(0, 650, 255, 0, flexiforce_reading, 2);
+  
+  strip.setPixelColor(0, Wheel(color));
+  strip.show();
   
   if (DEBUG) {
     Serial.print("force: ");
     Serial.print(flexiforce_reading);
-    Serial.print("delay:");
-    Serial.println(blink_delay);
+    Serial.print(" color:");
+    Serial.println(color);
   }
 
   digitalWrite(test_led, HIGH);
-  delay(blink_delay);
+  delay(wait);
 
   digitalWrite(test_led, LOW);
-  delay(blink_delay);
-
-  //digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //delay(1000);               // wait for a second
-  //digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  //delay(1000);               // wait for a second
 }
 
 
@@ -120,3 +128,31 @@ newEnd, float inputValue, float curve){
 
   return rangedValue;
 }
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
