33943b95599e58a728a882b731337de67c28a7dd
Josh Lind <jlind@tableausoftware.com>
2014-05-07 23:41:26 -0700

    use a strip down fork of the WS2801 library

--- Blink_Trinket.ino
+++ Blink_Trinket.ino
@@ -1,21 +1,26 @@
 /**
  * Sense and blink.
  */
+ 
+#include "Adafruit_WS2801.h"
 
 // I/O.
-int test_led = 1;        // Trinket built in LED is 1, standard Arduinos use 13.
-int PIN_IN_ONE = 2;      // Input for force: front.
-int ANALOG_READ_ONE = 1; // Analog input for force: front.
-int PIN_DATA = 0;        // Digital data out to lights.
-int PIN_CLOCK = 1;       // Digital clock out to lights.
+const int test_led = 1;        // Trinket built in LED is 1, standard Arduinos use 13.
+const int PIN_IN_ONE = 2;      // Input for force: front.
+const int ANALOG_READ_ONE = 1; // Analog input for force: front.
+const int PIN_DATA = 0;        // Digital data out to lights.
+const int PIN_CLOCK = 1;       // Digital clock out to lights.
 
 // Manage strip mangually.
 const int strip_length = 10;
-unsigned int strip[strip_length];
+Adafruit_WS2801 strip = Adafruit_WS2801(strip_length, PIN_DATA, PIN_CLOCK);
 
 // Program.
-int wait = 300;
-int force_curve = 8;
+const int wait = 300;
+const int force_curve = 8;
+
+// Utility.
+const int WHEEL_MAX = 255;
 
 // Basic setup.
 void setup() {                
@@ -31,14 +36,13 @@ void loop() {
   //float flexiforce_reading = 1.0;
   int flexiforce_reading = analogRead(ANALOG_READ_ONE);
   flexiforce_reading = flexiforce_reading - (flexiforce_reading % 50);
-  unsigned int color = fscale(0, 650, 96, 0, flexiforce_reading, force_curve);
-  float blink_delay = fscale(0, 1023, 400, 10, flexiforce_reading, force_curve);
+  unsigned int color = fscale(0, 650, WHEEL_MAX, 0, flexiforce_reading, force_curve);
+  int blink_delay = fscale(0, 1023, 400, 10, flexiforce_reading, force_curve);
 
   for (int i; i < strip_length; i++) {
-    //int this_color = color + (i * 3);
-    strip[i] = wheel(color);
+    strip.setPixelColor(i, Wheel(color));
   }
-  show();
+  strip.show();
 
   digitalWrite(test_led, HIGH);
   delay(blink_delay);
@@ -52,100 +56,43 @@ void loop() {
  */
 
 /**
- * Send color values from pixel array to led strip.
+ * Create a 24 bit color value from R,G,B
  */
-void show() {
-  unsigned int i, b;
-  // Pass through lights.
-  // http://playground.arduino.cc/Code/BitMath#registers
-  for (i=0; i < strip_length; i++) {
-    digitalWrite(PIN_DATA, HIGH);
-    //bitWrite(PORTD, PIN_DATA, HIGH);
-    digitalWrite(PIN_CLOCK, HIGH);
-    //bitWrite(PORTD, PIN_CLOCK, HIGH);
-    digitalWrite(PIN_CLOCK, LOW);
-    //bitWrite(PORTD, PIN_CLOCK, LOW);
-
-    // Bit shift loop.
-    for (b = 0x4000; b; b >>= 1) {
-      if (strip[i] & b) {
-        digitalWrite(PIN_DATA, HIGH);
-        //bitWrite(PORTD, PIN_DATA, HIGH);
-      }
-      else {
-        digitalWrite(PIN_DATA, LOW);
-        //bitWrite(PORTD, PIN_DATA, LOW);
-      }
-      digitalWrite(PIN_CLOCK, HIGH);
-      //bitWrite(PORTD, PIN_CLOCK, HIGH);
-      digitalWrite(PIN_CLOCK, LOW);
-      //bitWrite(PORTD, PIN_CLOCK, LOW);
-    }
-  }
-
-  latch_leds(strip_length);
+uint32_t Color(byte r, byte g, byte b) {
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
 }
 
 /**
- * Activate new color pattern in LED strip.
+ * Input a value 0 to 255 to get a color value.
+ * The colours are a transition r - g -b - back to r
  */
-void latch_leds(int n) {
-  digitalWrite(PIN_DATA, LOW);
-  //bitWrite(PORTD, PIN_DATA, LOW);
-  for (int i = 8 * n; i > 0; i--) {
-    digitalWrite(PIN_CLOCK, HIGH);
-    //bitWrite(PORTD, PIN_CLOCK, HIGH);
-    digitalWrite(PIN_CLOCK, LOW);
-    //bitWrite(PORTD, PIN_CLOCK, LOW);
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-}
-
-/**
- * Set color value as RGB argument, values = 0..31
- */
-unsigned int color(int r, int g, int b) {
-  r = r & 0x1F;
-  g = g & 0x1F;
-  b = b & 0x1F;
-  return  (b << 10) | (r << 5) | g;
-}
-
-/**
- * Colorwheel with 96 colors, 0..95
- */
-unsigned int wheel(int WheelPos) {
-  byte r, g, b;
-  switch (WheelPos / 32) {
-  case 0:
-    r = 31 - WheelPos % 32; // Red down
-    g = WheelPos % 32;      // Green up
-    b = 0;                  // blue off
-    break;
-  case 1:
-    g = 31 - WheelPos % 32; // green down
-    b = WheelPos % 32;      // blue up
-    r = 0;                  // red off
-    break;
-  case 2:
-    b = 31 - WheelPos % 32; // blue down
-    r = WheelPos % 32;      // red up
-    g = 0;                  // green off
-    break;
-  }
-
-  return(color(r,g,b));
 }
 
 /**
  * Map with curve.
  */
-float fscale( float originalMin, float originalMax, float newBegin, float newEnd, float inputValue, float curve){
-
-  float OriginalRange = 0;
-  float NewRange = 0;
-  float zeroRefCurVal = 0;
-  float normalizedCurVal = 0;
-  float rangedValue = 0;
+int fscale(int originalMin, int originalMax, int newBegin, int newEnd, int inputValue, int curve){
+  int OriginalRange = 0;
+  int NewRange = 0;
+  int zeroRefCurVal = 0;
+  int normalizedCurVal = 0;
+  int rangedValue = 0;
   boolean invFlag = 0;
 
   // condition curve parameter
@@ -156,11 +103,6 @@ float fscale( float originalMin, float originalMax, float newBegin, float newEnd
 
   curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output
   curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
-
-  /*
-   Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
-   Serial.println();
-   */
 
   // Check for out of range inputValues
   if (inputValue < originalMin) {
@@ -173,11 +115,10 @@ float fscale( float originalMin, float originalMax, float newBegin, float newEnd
   // Zero Refference the values
   OriginalRange = originalMax - originalMin;
 
-  if (newEnd > newBegin){
+  if (newEnd > newBegin) {
     NewRange = newEnd - newBegin;
   }
-  else
-  {
+  else {
     NewRange = newBegin - newEnd;
     invFlag = 1;
   }
@@ -185,27 +126,17 @@ float fscale( float originalMin, float originalMax, float newBegin, float newEnd
   zeroRefCurVal = inputValue - originalMin;
   normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
 
-  /*
-  Serial.print(OriginalRange, DEC);  
-   Serial.print("   ");  
-   Serial.print(NewRange, DEC);  
-   Serial.print("   ");  
-   Serial.println(zeroRefCurVal, DEC);  
-   Serial.println();  
-   */
-
   // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine
-  if (originalMin > originalMax ) {
+  if (originalMin > originalMax) {
     return 0;
   }
 
-  if (invFlag == 0){
-    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+  if (invFlag == 0) {
+    rangedValue = (pow(normalizedCurVal, curve) * NewRange) + newBegin;
 
   }
-  else     // invert the ranges
-  {  
-    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange);
+  else {  
+    rangedValue = newBegin - (pow(normalizedCurVal, curve) * NewRange);
   }
 
   return rangedValue;
